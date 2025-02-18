dc869ebffd0f5b1178c928b60246a710da760892
xlezzerx <xlezzerx@users.noreply.github.com>
2025-02-18 17:31:18 +0000

    deploy: 7eae859e29731b07877ca08d6cccd9690f8745a1

---
+++ script.js
@@ -0,0 +1,44 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-analytics.js";
+// Get references to DOM elements
+const eventForm = document.getElementById("event-form");
+const eventTitle = document.getElementById("event-title");
+const eventDate = document.getElementById("event-date");
+const eventTime = document.getElementById("event-time");
+const eventsList = document.getElementById("events");
+
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyBFlui1di6EVCFZpZFSxrMr5dhiK3lQrGM",
+  authDomain: "event-tracker-f54eb.firebaseapp.com",
+  projectId: "event-tracker-f54eb",
+  storageBucket: "event-tracker-f54eb.firebasestorage.app",
+  messagingSenderId: "730198827128",
+  appId: "1:730198827128:web:7f628944530a5c6eb84d8d",
+  measurementId: "G-KVH5MRJLWP",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Event handler for form submission
+eventForm.addEventListener("submit", function (e) {
+  e.preventDefault(); // Prevent the default form submission
+
+  const title = eventTitle.value;
+  const date = eventDate.value;
+  const time = eventTime.value;
+
+  if (title && date && time) {
+    // Create new event element
+    const eventItem = document.createElement("li");
+    eventItem.textContent = `${title} - ${date} ${time}`;
+
+    // Append event to the list
+    eventsList.appendChild(eventItem);
+
+    // Clear the form
+    eventForm.reset();
+  }
+});
