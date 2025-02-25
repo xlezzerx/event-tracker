dca0c4454c5d126edb82a2186f60f710b2c66286
xlezzerx <151861662+xlezzerx@users.noreply.github.com>
2025-02-25 15:23:46 -0500

    please...

--- scripts/auth.js
+++ scripts/auth.js
@@ -157,6 +157,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+auth.useDeviceLanguage();
+auth.setPersistence(browserSessionPersistence);
 
 console.log("Firebase initialized:", app);
 console.log("Auth initialized:", auth);
